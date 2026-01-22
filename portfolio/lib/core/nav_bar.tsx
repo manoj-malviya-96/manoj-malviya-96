e1c9c45d22f4f1b46727bd6d4027645786992e5e
manoj <malviyamanoj1896@gmail.com>
2026-01-22 19:58:15 +0100

    do some magic here

--- portfolio/lib/core/nav_bar.tsx
+++ portfolio/lib/core/nav_bar.tsx
@@ -8,7 +8,6 @@ import { usePathname } from "next/navigation";
 import useScrollVisibility from "@/lib/ui/scroll_visibility";
 import Link from "@/lib/ui/link";
 import { ExternalURL } from "@/lib/types";
-import { useMemo } from "react";
 
 function MainLogo() {
   return (
@@ -56,20 +55,15 @@ function CTALink() {
 export default function Navbar() {
   const pathname = usePathname();
 
-  const { isVisible, isAtTop } = useScrollVisibility({
+  const { isVisible } = useScrollVisibility({
     velocityThreshold: 0.8,
   });
-
-  const hideBackgroundWhenOnTop = useMemo(
-    () => pathname === "/" && isAtTop,
-    [isAtTop, pathname],
-  );
 
   return (
     <nav
       className={mergeCls(
         "fixed top-0 left-0 w-full h-12 z-10 p-1 duration-300 transition-transform",
-        hideBackgroundWhenOnTop ? "bg-transparent" : "bg-back", // Only transparent on home
+        pathname === "/" ? "bg-transparent" : "bg-back", // Only transparent on home
         isVisible ? "translate-y-0" : "-translate-y-full",
       )}
       data-theme="dark"
