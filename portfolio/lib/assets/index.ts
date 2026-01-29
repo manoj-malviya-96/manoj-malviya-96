fab2298065f8279cf51d01d65091306a76e1e67d
manoj <malviyamanoj1896@gmail.com>
2026-01-29 20:01:15 +0100

    remove

--- portfolio/lib/assets/index.ts
+++ portfolio/lib/assets/index.ts
@@ -1,6 +1,5 @@
-export { default as FlowkeyLogo } from "./flowkey-dark.png";
-export { default as FormlabsLogo } from "./formlabs-black.png";
-export { default as IITJLogo } from "./iitj.png";
-export { default as UserAvatar } from "./manoj-1.png";
-export { default as NoahLabs } from "./noahlabs-logo.jpeg";
-export { default as PennStateLogo } from "./pennstate-blue.png";
+export {default as FlowkeyLogo} from "./flowkey-dark.png";
+export {default as FormlabsLogo} from "./formlabs-black.png";
+export {default as IITJLogo} from "./iitj.png";
+export {default as UserAvatar} from "./manoj-1.png";
+export {default as PennStateLogo} from "./pennstate-blue.png";
