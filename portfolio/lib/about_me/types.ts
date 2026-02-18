6217c123662921c154bccf0e68d22b285503a153
manoj <malviyamanoj1896@gmail.com>
2026-02-18 19:22:40 +0100

    add topopt

--- portfolio/lib/about_me/types.ts
+++ portfolio/lib/about_me/types.ts
@@ -1,38 +1,40 @@
 export type SoftwareConcepts =
-	| "web"
-	| "mobile"
-	| "ai"
-	| "rendering"
-	| "open-source"
-	| "high-performance"
-	| "gpu"
-	| "optimization"
-	| "cad"
-	| "micro-services";
+    | "web"
+    | "mobile"
+    | "ai"
+    | "rendering"
+    | "open-source"
+    | "high-performance"
+    | "gpu"
+    | "optimization"
+    | "cad"
+    | "simulation"
+    | "micro-services";
 
 export type SoftSkills =
-	| "communication"
-	| "ui/ux"
-	| "project-management"
-	| "devops"
-	| "testing";
+    | "communication"
+    | "ui/ux"
+    | "project-management"
+    | "devops"
+    | "testing";
 
 export type ProgrammingFrameworks =
-	| "react"
-	| "nextjs"
-	| "qt/qml"
-	| "tailwind"
-	| "vtk"
-	| "numpy"
-	| "pytorch"
-	| "tensorflow"
-	| "wasm"
-	| "opengl";
+    | "react"
+    | "nextjs"
+    | "qt/qml"
+    | "tailwind"
+    | "vtk"
+    | "numpy"
+    | "pytorch"
+    | "tensorflow"
+    | "wasm"
+    | "threejs"
+    | "opengl";
 
 export type ProgrammingLanguage =
-	| "typescript"
-	| "python"
-	| "rust"
-	| "go"
-	| "c++"
-	| "swift";
+    | "typescript"
+    | "python"
+    | "rust"
+    | "go"
+    | "c++"
+    | "swift";
--- portfolio/lib/projects/list/index.ts
+++ portfolio/lib/projects/list/index.ts
@@ -1,11 +1,13 @@
-import { project as HoneycombProject } from "./honeycomb";
-import { project as MuvizProject } from "./muviz";
-import { project as PortfolioProject } from "./portfolio";
-import type { Project } from "./types";
+import {project as HoneycombProject} from "./honeycomb";
+import {project as MuvizProject} from "./muviz";
+import {project as PortfolioProject} from "./portfolio";
+import {project as TopOptProject} from "./topopt";
+import type {Project} from "./types";
 
 export const ALL_PROJECTS: Project[] = [
-	PortfolioProject,
-	MuvizProject,
-	HoneycombProject,
-	// BlackholeProject,
+    PortfolioProject,
+    MuvizProject,
+    HoneycombProject,
+    TopOptProject,
+    // BlackholeProject,
 ] as const;
