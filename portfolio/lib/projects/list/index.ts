672e234fcb22b4f3877cde798d957b1679c91b5e
manoj <malviyamanoj1896@gmail.com>
2026-09-01 11:17:53 +0200

    Add topopt to the project lineup

--- portfolio/lib/projects/list/index.ts
+++ portfolio/lib/projects/list/index.ts
@@ -1,11 +1,13 @@
 import { project as HoneycombProject } from "./honeycomb";
 import { project as MuvizProject } from "./muviz";
 import { project as PortfolioProject } from "./portfolio";
+import { project as TopOptProject } from "./topopt";
 import type { Project } from "./types";
 
 export const ALL_PROJECTS: Project[] = [
 	PortfolioProject,
 	MuvizProject,
 	HoneycombProject,
+	TopOptProject,
 	// BlackholeProject,
 ] as const;
