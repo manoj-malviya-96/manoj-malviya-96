c4871a3dc0d3a8f8f3be8ec80c4d5fbdbac1c5b7
manoj <malviyamanoj1896@gmail.com>
2026-09-01 13:12:29 +0200

    replace WebGL background with a lightweight mesh canvas

--- portfolio/lib/ui/index.ts
+++ portfolio/lib/ui/index.ts
@@ -1,4 +1,5 @@
 export { default as Icon } from "./icon";
 export { default as Link } from "./link";
-export { default as NeuralCanvas } from "./neural_bg";
+export { default as MeshCanvas } from "./mesh_canvas";
 export { default as StatCard } from "./stat";
+export { default as ThemeToggle } from "./theme_toggle";
