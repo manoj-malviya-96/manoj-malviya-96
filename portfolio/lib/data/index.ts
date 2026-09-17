9a008a6a9b8f36bb4bd7c95c90f80a564f8912e6
manoj <malviyamanoj1896@gmail.com>
2026-09-17 20:11:37 +0200

    Add filed patent data

--- portfolio/lib/data/index.ts
+++ portfolio/lib/data/index.ts
@@ -3,6 +3,7 @@ export * from "@/lib/data/degrees";
 export * from "@/lib/data/github";
 export * from "@/lib/data/google_scholar";
 export * from "@/lib/data/organizations";
+export * from "@/lib/data/patents";
 export * from "@/lib/data/profile";
 export * from "@/lib/data/projects";
 export * from "@/lib/data/truss_opt";
