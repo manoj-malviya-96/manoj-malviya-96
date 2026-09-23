c599519a66fb95a97a81468c80b753c4dd0fed54
manoj <malviyamanoj1896@gmail.com>
2026-09-23 09:26:48 +0200

    work page finally gets a headline

--- portfolio/app/work/page.tsx
+++ portfolio/app/work/page.tsx
@@ -1,21 +1,19 @@
 "use client";
 
-import { Flex, Grid } from "@manoj-malviya-96/atom";
+import { Flex, Grid, Text } from "@manoj-malviya-96/atom";
 import { WorkItems } from "@/lib/data";
 import { Eyebrow, Link } from "@/lib/shared";
 import WorkCard from "@/lib/work/work_card";
 
 export default function WorkPage() {
 	return (
-		<Flex direction="col" width="content">
+		<Flex direction="col" gap="lg" width="content">
+			<Text variant="hero">Selected work.</Text>
 			<WorkToc />
-			<Flex as="article" direction="col" gap="xs" width="full">
-				<Eyebrow>Selected work</Eyebrow>
-				<Flex direction="col" gap="xl" width="full">
-					{WorkItems.map((item) => (
-						<WorkCard key={item.id} item={item} />
-					))}
-				</Flex>
+			<Flex as="article" direction="col" gap="xl" width="full">
+				{WorkItems.map((item) => (
+					<WorkCard key={item.id} item={item} />
+				))}
 			</Flex>
 		</Flex>
 	);
